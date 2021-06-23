e99de188e84d18284d3650a029d80f357fdb4f38
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-23 17:50:18 +0000

    trailing spaces

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function comparision () {
     document.getElementById('answer').innerHTML = 'You can see an R  movie or an NC-17 movie alone'
   } else if (age >= 13) {
     document.getElementById('answer').innerHTML = 'You can see a PG-13, PG or G movie alone'
-  } else if (age >=4 ) {
+  } else if (age >= 4) {
     document.getElementById('answer').innerHTML = 'You can see a G or PG  movie alone'
   } else {
     document.getElementById('answer').innerHTML = 'You are less than 4 years old. How did you get on this website ?'
